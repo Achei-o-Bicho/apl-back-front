090387aa32f6b385660ccbc142c7ce7771d1bcf4
Patrick Lessa Teixeira <patrick@pop-os.localdomain>
2023-06-06 20:37:51 -0300

    Add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersController } from './modules/users/users.controller';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.DB_URL)
+    MongooseModule.forRoot(process.env.DB_URL),
+    UsersModule
   ],
-  controllers: [AppController],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
