732bf6beb031cbffdf98c0b2d6a5190d6fcb8fd7
José Paulo <jose.silva@bandtec.com.br>
2023-06-09 19:40:22 -0300

    add(): endpoints de busca de pets

--- src/modules/pets/pets.controller.ts
+++ src/modules/pets/pets.controller.ts
@@ -1,9 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
+import { PetsService } from './pet.service';
+import { Response } from 'express';
 
 @Controller('pets')
 export class PetsController {
+  constructor(private petService: PetsService) {}
+  @Get(':/id')
+  public async getPetById(@Param('id') id: string, @Res() res: Response) {
+    const petFounded = await this.petService.findAllById(id);
+
+    if (!petFounded) res.status(HttpStatus.NOT_FOUND);
+
+    res.status(HttpStatus.OK).json(petFounded);
+  }
+
   @Get()
-  public async getPetById(): Promise<string> {
-    return 'Olá';
+  public async getAllPets(@Res() res: Response) {
+    const pets = await this.petService.findAll();
+
+    if (!pets) res.status(HttpStatus.NO_CONTENT);
+
+    res.status(HttpStatus.OK).json(pets);
   }
 }
