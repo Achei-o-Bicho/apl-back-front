7e7cab480002b8ff99334cb3db96e890b16a9c98
José Paulo <jose.silva@bandtec.com.br>
2023-06-09 18:01:53 -0300

    refactor(): alterando maneira de pegar as variaveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PetsModule } from './modules/pets/pet.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     PetsModule,
     ConfigModule.forRoot(),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get('DATABASE_URL'),
-      }),
-      inject: [ConfigService],
-    }),
+    MongooseModule.forRoot(process.env.DB_URL),
   ],
   controllers: [],
   providers: [],
