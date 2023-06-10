fd5d1a6bc5a8f03a8aa8158460dfa810087a851c
José Paulo <jose.silva@bandtec.com.br>
2023-06-10 13:23:09 -0300

    add(): adicionando estratégia para autoincrement no petId

--- src/modules/pets/pet.schema.ts
+++ src/modules/pets/pet.schema.ts
@@ -2,9 +2,14 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { AnimalGender } from './enum/animalGender.enum';
 import { TypePet } from './enum/typePet.enum';
 import { HydratedDocument } from 'mongoose';
+import * as mongoose from 'mongoose';
+import * as autoIncrement from 'mongoose-plugin-autoinc';
 
 @Schema()
 export class Pet {
+  @Prop({ unique: true })
+  petId: number;
+
   @Prop({ required: true })
   name: string;
 
@@ -29,3 +34,12 @@ export class Pet {
 
 export const PetSchema = SchemaFactory.createForClass(Pet);
 export type PetDocument = HydratedDocument<Pet>;
+
+PetSchema.plugin(autoIncrement.plugin, {
+  model: 'Pet',
+  field: 'petId',
+  startAt: 134548,
+  incrementBy: 1,
+});
+
+export const PetModel = mongoose.model<PetDocument>('Pet', PetSchema);
