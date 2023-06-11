ccac4a88904536f9aff253a7a6cff5e9e0be181d
José Paulo <jose.silva@bandtec.com.br>
2023-06-11 00:45:39 -0300

    add(): adicionando logger

--- src/modules/pets/pet.service.ts
+++ src/modules/pets/pet.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pet } from './pet.schema';
 import { Model } from 'mongoose';
@@ -6,22 +6,27 @@ import { CreatePetDto } from './dto/pet.dto';
 
 @Injectable()
 export class PetsService {
+  private readonly logger = new Logger(PetsService.name);
   constructor(@InjectModel(Pet.name) private petModel: Model<Pet>) {}
 
   async create(createPetDto: CreatePetDto): Promise<Pet> {
     const createdPet = new this.petModel(createPetDto);
+    this.logger.log('Creating Pet');
     return await createdPet.save();
   }
 
   async findAll(): Promise<Pet[]> {
+    this.logger.log('Getting All Pets');
     return this.petModel.find().exec();
   }
 
   async findAllById(petId: string): Promise<Pet> {
+    this.logger.log('Getting Pet by id:' + petId);
     return this.petModel.findById(petId);
   }
 
   async removeById(petId: string): Promise<void> {
+    this.logger.log('Removing Pet by id:' + petId);
     await this.petModel.findByIdAndRemove(petId);
   }
 }
