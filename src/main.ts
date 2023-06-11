3db295a6729e56e915a2dd856fb964f3de9075cc
José Paulo <jose.silva@bandtec.com.br>
2023-06-11 02:36:42 -0300

    add(): adicionando pipeline para validar classes de dtos quando chegarem da requisição

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('apl-back-front/v1');
+
+  app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors();
   await app.listen(process.env.PORT);
