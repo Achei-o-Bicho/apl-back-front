321a3fb9890e6041bc8c4d2a5f4a1e0c86d55888
José Paulo <jose.silva@bandtec.com.br>
2023-06-12 22:44:04 -0300

    add(): configurando modulo para o mongoose

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { User, UserSchema } from './schema/user.schema';
 
 @Module({
-  providers: [UsersService]
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema, collection: 'COLLECTION_USER' },
+    ]),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class UsersModule {}
