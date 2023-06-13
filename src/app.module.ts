44be97e90eea720d146de0e071ff3af95d0ef796
José Paulo <jose.silva@bandtec.com.br>
2023-06-12 22:43:45 -0300

    remove(): removendo dependencias para não causar circulação de dependencias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersController } from './modules/users/users.controller';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DB_URL),
-    UsersModule
+    UsersModule,
   ],
-  controllers: [AppController, UsersController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
