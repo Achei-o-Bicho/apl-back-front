e1e8374b7f3ba70344f4d226d799818211ac78c7
José Paulo <jose.silva@bandtec.com.br>
2023-06-12 22:45:51 -0300

    add(): colocando .exec no final do findAll e o eslint formata tudo

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -7,34 +7,42 @@ import { User } from './schema/user.schema';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>){}
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  async create(createUserDto: CreateUserDto): Promise<IUser>{
+  async create(createUserDto: CreateUserDto): Promise<IUser> {
     const createdUser = new this.userModel(createUserDto);
     return createdUser.save();
   }
 
-  async findAll(): Promise<IUser[]>{
-    const userData = await this.userModel.find();
-    if(userData.length == 0 || !userData) throw new NotFoundException('Users data not found');
+  async findAll(): Promise<IUser[]> {
+    const userData = await this.userModel.find().exec();
+    if (userData.length == 0 || !userData)
+      throw new NotFoundException('Users data not found');
     return userData;
   }
 
-  async findById(userId: string): Promise<IUser>{
-    const userData =  await this.userModel.findById(userId);
-    if(!userData) throw new NotFoundException(`User ${userId} not found`);
+  async findById(userId: string): Promise<IUser> {
+    const userData = await this.userModel.findById(userId);
+    if (!userData) throw new NotFoundException(`User ${userId} not found`);
     return userData;
   }
 
-  async updateUserById(userId: string, updateStudentDto: CreateUserDto): Promise<IUser>{
-    const existingUser = await this.userModel.findByIdAndUpdate(userId, updateStudentDto, {new: true})
-    if(!existingUser) throw new NotFoundException(`User ${userId} not found`);
+  async updateUserById(
+    userId: string,
+    updateStudentDto: CreateUserDto,
+  ): Promise<IUser> {
+    const existingUser = await this.userModel.findByIdAndUpdate(
+      userId,
+      updateStudentDto,
+      { new: true },
+    );
+    if (!existingUser) throw new NotFoundException(`User ${userId} not found`);
     return existingUser;
   }
 
-  async deleteUserById(userId: string): Promise<IUser>{
+  async deleteUserById(userId: string): Promise<IUser> {
     const deletedUser = await this.userModel.findByIdAndDelete(userId);
-    if(!deletedUser) throw new NotFoundException(`User ${userId} not found`);
+    if (!deletedUser) throw new NotFoundException(`User ${userId} not found`);
     return deletedUser;
   }
 }
