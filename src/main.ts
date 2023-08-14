62c28b4ab663bd710a58e50fad5fcf1212534296
José Paulo <jose.silva@bandtec.com.br>
2023-08-14 20:27:05 -0300

    add: adicionando módulo do swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('BFF do Projeto Achei o Bicho')
+    .setDescription('Serviço para gerenciamento de usuários e pets')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+
   app.setGlobalPrefix(process.env.GLOBAL_PATH);
 
   app.useGlobalPipes(new ValidationPipe());
