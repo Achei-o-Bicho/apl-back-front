2a362d59171ad2b5aa8b5a262bd0f7336fcf6249
José Paulo <jose.silva@bandtec.com.br>
2023-08-16 21:07:59 -0300

    refactor: organizando melhor as classes

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from './jwt-strategy';
+import { JwtStrategy } from './strategy/jwt-strategy';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { ConfigModule } from '@nestjs/config';
