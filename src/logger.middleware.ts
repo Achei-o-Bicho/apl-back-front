aa030df27f18921a62f6994dfa3c3fff03ee21e9
José Paulo <jose.silva@bandtec.com.br>
2023-08-17 01:29:35 -0300

    enhancement: melhorando logs de http request e response

--- src/logger.middleware.ts
+++ src/logger.middleware.ts
@@ -1,13 +1,25 @@
-import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   private logger = new Logger(`HTTP`);
   use(req: Request, res: Response, next: NextFunction) {
-    this.logger.log(
-      `Logging HTTP request ${req.method} ${req.originalUrl} ${res.statusCode}`,
-    );
+    this.logger.log(`Logging HTTP request ${req.method} ${req.originalUrl}`);
+
+    const originalSend = res.send;
+    res.send = (body: any): Response => {
+      const statusCode = res.statusCode;
+
+      if (statusCode === HttpStatus.OK)
+        this.logger.log(`Logging HTTP Response ${statusCode}`);
+      else if (statusCode === HttpStatus.INTERNAL_SERVER_ERROR)
+        this.logger.error(`Logging HTTP Response ${statusCode}`);
+      else this.logger.debug(`Logging HTTP Response ${statusCode}`);
+
+      return originalSend.call(res, body);
+    };
+
     next();
   }
 }
