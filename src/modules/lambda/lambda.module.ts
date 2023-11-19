040e2536270860f1871a2a29dee4d32573819c18
José Paulo <jose.silva@bandtec.com.br>
2023-11-19 02:57:09 -0300

    chore(): importando modulo de feature flag

--- src/modules/lambda/lambda.module.ts
+++ src/modules/lambda/lambda.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LambdaService } from './lambda.service';
 import { HttpModule } from '@nestjs/axios';
+import { FeatureFlagModule } from '../feature-flag/feature-flag.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, FeatureFlagModule],
   exports: [LambdaService],
   providers: [LambdaService],
 })
