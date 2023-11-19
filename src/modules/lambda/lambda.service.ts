985c37e40bca152b3845adf814f67866b54933f3
José Paulo <jose.silva@bandtec.com.br>
2023-11-19 02:58:12 -0300

    chore(): adicionando lógica para ver se flag está ligada e revendo lógica para salvar imagens no s3

--- src/modules/lambda/lambda.service.ts
+++ src/modules/lambda/lambda.service.ts
@@ -1,11 +1,23 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable, Logger } from '@nestjs/common';
-import { AxiosRequestConfig } from 'axios';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AxiosError, AxiosRequestConfig } from 'axios';
+import { catchError, lastValueFrom, map } from 'rxjs';
+import { FeatureFlagService } from '../feature-flag/feature-flag.service';
 
 @Injectable()
 export class LambdaService {
   private logger = new Logger(LambdaService.name);
-  constructor(private httpService: HttpService) {}
+  constructor(
+    private httpService: HttpService,
+    private readonly featureFlagService: FeatureFlagService,
+  ) {}
 
   async sendImageToGoogleFunction(
     fileBuffer: Buffer | string,
@@ -13,6 +25,15 @@ export class LambdaService {
     contentType: string,
   ): Promise<ISaveImageGoogleCloudFunction> {
     try {
+      const sendImageIsEnabled = (
+        await this.featureFlagService.get('SEND_IMAGE_LAMBDA')
+      ).isEnabled;
+
+      if (!sendImageIsEnabled) {
+        this.logger.warn('Flag send image is lambda is not enabled');
+        return null;
+      }
+
       const url =
         'https://us-central1-projeto-dog-extract.cloudfunctions.net/uploadImagePetMongoDbfFunction';
 
@@ -30,13 +51,16 @@ export class LambdaService {
 
       this.logger.log('Sending data to cloud function');
 
-      const response = await this.httpService
-        .post(url, data, config)
-        .toPromise();
+      const response = await lastValueFrom(
+        this.httpService.post(url, data, config).pipe(
+          map((resp) => resp),
+          catchError((error: AxiosError) => {
+            console.error('Erro na requisição HTTP:', error);
 
-      if (response.status !== 200) {
-        this.logger.error('Error to send data for Cloud Function');
-      }
+            throw new HttpException('Erro na requisição HTTP', error.status);
+          }),
+        ),
+      );
 
       return response.data as ISaveImageGoogleCloudFunction;
     } catch (error) {
